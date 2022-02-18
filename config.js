b6658a16768b8ad2577deee470e7a5dd6fcd61d8
pawelmat142 <pawelmat142@gmail.com>
2022-02-18 18:05:29 +0100

    trying to remote db connect - production

--- config.js
+++ config.js
@@ -7,10 +7,10 @@ module.exports = {
     db_config: {
         client: 'mysql2',
         connection: {
-            host: 'localhost',
-            user: 'root',
-            password: '',
-            database: 'spins'
+            host: process.env.DB_HOST,
+            user: process.env.DB_USER,
+            password: process.env.DB_PASSWORD,
+            database: process.env.DB_NAME
         }
     }
     
