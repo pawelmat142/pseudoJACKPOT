cf38a8e7d3e45e1dcb61e59824e93cdfd825ee70
pawelmat142 <pawelmat142@gmail.com>
2022-05-24 21:27:27 +0200

    API changes to make it faster

--- config.js
+++ config.js
@@ -8,20 +8,20 @@ module.exports = {
         client: 'mysql2',
 
         // REMOTE
-        // connection: {
-        //     host: process.env.DB_HOST,
-        //     user: process.env.DB_USER,
-        //     password: process.env.DB_PASSWORD,
-        //     database: 'spins'
-        // }
-
-        // LOCAL
         connection: {
-            host: 'localhost',
-            user: 'root',
-            password: '',
+            host: process.env.DB_HOST,
+            user: process.env.DB_USER,
+            password: process.env.DB_PASSWORD,
             database: 'spins'
         }
+
+        // LOCAL
+        // connection: {
+        //     host: 'localhost',
+        //     user: 'root',
+        //     password: '',
+        //     database: 'spins'
+        // }
     }
     
 }
